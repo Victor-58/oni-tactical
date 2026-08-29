a6a5a0597c60ba49a0df8d7aa59298564c2376e0
Victor De Souza Pereira <victor.spereira@aircompany.ai>
2026-08-28 21:56:40 -0300

    aplica carrossel com setas em instrutores, adiciona 4o instrutor e protecao contra swiper lock

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://idyllic-kataifi-a55830.netlify.app',
-  integrations: [sitemap(), compress()],
+  integrations: [sitemap(), compress(), icon()],
 });
